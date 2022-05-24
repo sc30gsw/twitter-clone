0a320380715d7b2d1430fa50d506d155a4209be5
sc30gsw <haidoushi875@gmail.com>
2022-05-25 06:36:54 +0900

    Timelineのimport

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { FC } from "react";
 import "./App.css";
 import { Sidebar } from "./components/sidebar/Sidebar";
+import { Timeline } from "./components/timeline/Timeline";
 
 export const App: FC = () => {
 	return (
 		<div className="app">
+			{/* Sidebar */}
 			<Sidebar />
+
+			{/* Timeline */}
+			<Timeline />
+
+			{/* Widget */}
 		</div>
 	);
 };
