11b21c47e5f56e27b44640e5bc978ee5446b7239
sc30gsw <haidoushi875@gmail.com>
2022-05-25 06:37:16 +0900

    タイムラインのヘッダー部分の作成

---
+++ src/components/timeline/Timeline.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./css/Timeline.css";
+
+export const Timeline = () => {
+	return (
+		<div className="timeline">
+			{/* ヘッダー */}
+			<div className="timeline_header">
+				<h2>ホーム</h2>
+			</div>
+
+			{/* ツイートボックス */}
+
+			{/* 投稿情報 */}
+		</div>
+	);
+};
