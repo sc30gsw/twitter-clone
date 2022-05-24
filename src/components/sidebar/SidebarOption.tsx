58e06de53749a9eb6ae28d306f0b7226da8c95c0
sc30gsw <haidoushi875@gmail.com>
2022-05-25 06:10:37 +0900

    サイドバーオプションにCSSを適用する

--- src/components/sidebar/SidebarOption.tsx
+++ src/components/sidebar/SidebarOption.tsx
@@ -1,13 +1,14 @@
 import { SvgIconTypeMap } from "@mui/material";
 import { OverridableComponent } from "@mui/material/OverridableComponent";
 import React, { FC } from "react";
+import "./css/SidebarOption.css";
 
 export const SidebarOption: FC<{
 	text: string;
 	Icon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
 }> = ({ text, Icon }) => {
 	return (
-		<div>
+		<div className="sidebarOption">
 			<Icon />
 			<h2>{text}</h2>
 		</div>
