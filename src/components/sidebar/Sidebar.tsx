03388fe1407590223ab6a6404562d621e8484abe
sc30gsw <haidoushi875@gmail.com>
2022-05-25 05:38:19 +0900

    ツイッターアイコンの表示

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+export const Sidebar = () => {
+	return (
+		<div className="sidebar">
+			{/* ツイッターアイコン */}
+			<TwitterIcon className="sidebar_twitterIcon" />
+
+			{/* サイドバーオプション */}
+
+			{/* ツイートボタン */}
+		</div>
+	);
+};
